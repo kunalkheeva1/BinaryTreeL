b01d644511c1ee6b1ea1164ddb9f7f0ebe198704
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-27 19:37:47 -0700

    created a node with left and right nodes which contains data

--- src/BinaryTreeL.java
+++ src/BinaryTreeL.java
@@ -1,2 +1,18 @@
 public class BinaryTreeL {
+
+    //creating a node class to work with nodes in my code of trees
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left =null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
