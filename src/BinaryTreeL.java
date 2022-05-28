aebef5b408cb3e27b4764c741391cbd169d24c58
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-27 20:06:56 -0700

    created a class in it to build the tree and provided the condition of empty tree

--- src/BinaryTreeL.java
+++ src/BinaryTreeL.java
@@ -11,6 +11,20 @@ public class BinaryTreeL {
             this.right = null;
         }
     }
+    //Now creating a binary tree class and storing a recursive function in it
+    static class BinaryTree{
+        static int idx= -1;
+        static Node buildTree(int[] nodes){
+            idx++;
+            if(nodes[idx]==-1){             //in case there is no element contained by the tree
+                return null;
+            }
+
+
+
+        }
+    }
+
 
     public static void main(String[] args) {
 
