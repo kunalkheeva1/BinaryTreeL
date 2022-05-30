c5533eb9f5098127b4ac194ef231fa9bcce869f9
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-29 19:51:29 -0700

    created a method for Inorder traversing so that it prints the left nodes first

--- src/BinaryTreeL.java
+++ src/BinaryTreeL.java
@@ -33,16 +33,27 @@ public class BinaryTreeL {
             return;
         }
 
-        System.out.println(root.data);          //here i will be priniting the root node first
+        System.out.print(root.data+ " ");          //here i will be priniting the root node first
         preOrder(root.left);                    //Now it rucursively prints left nodes and then move on to right
         preOrder(root.right);
+    }
+
+    //creating a method to do inorder traversal which basically prints left nodes first
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);                     //going to to the leaf left
+        System.out.print(root.data+ " ");       //then printing
+        inOrder(root.right);                    //then recursively doing same in the right side
     }
 
     public static void main(String[] args) {
         int [] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        preOrder(root);
 
     }
+
 }
